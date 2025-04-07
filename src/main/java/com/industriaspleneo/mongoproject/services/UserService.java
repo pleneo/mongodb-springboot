d2067aaf18ab417ebe055c032a7ba9f672bce9f9
Plinio Gomes <plinioigt@gmail.com>
2025-04-07 16:40:54 -0300

    user findById and exception handlers

--- src/main/java/com/industriaspleneo/mongoproject/services/UserService.java
+++ src/main/java/com/industriaspleneo/mongoproject/services/UserService.java
@@ -2,11 +2,15 @@ package com.industriaspleneo.mongoproject.services;
 
 import com.industriaspleneo.mongoproject.entities.User;
 import com.industriaspleneo.mongoproject.repositories.UserRepository;
+import com.industriaspleneo.mongoproject.services.exceptions.ObjectNotFoundException;
+import com.sun.jdi.ObjectCollectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +20,10 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object with id = " + id + " not found."));
     }
 }
