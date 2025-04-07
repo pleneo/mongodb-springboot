2ce01e9dffcc8b31cac599fff29b0cc46690e047
Plinio Gomes <plinioigt@gmail.com>
2025-04-07 15:18:00 -0300

    created User resource, service and repository logic layers

---
+++ src/main/java/com/industriaspleneo/mongoproject/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.industriaspleneo.mongoproject.repositories;
+
+import com.industriaspleneo.mongoproject.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
---
+++ src/main/java/com/industriaspleneo/mongoproject/services/UserService.java
@@ -0,0 +1,20 @@
+package com.industriaspleneo.mongoproject.services;
+
+import com.industriaspleneo.mongoproject.entities.User;
+import com.industriaspleneo.mongoproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+}
